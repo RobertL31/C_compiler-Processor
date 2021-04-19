7813e3da0a1b361a8a89e515a547dde4a31de16a
AliciaCalmet <acalmet@etud.insa-toulouse.fr>
2021-04-19 10:31:38 +0200

    fin gestion des commentaires (ça marche !)

--- test.c
+++ test.c
@@ -1,11 +1,14 @@
-/*ceci est un commentaire*/
+/*ceci est un main*/
 int main(){
     int i;
     int j;
+    /*bouh affectation*/
     i = 2, j = 3, i = 3;
+    /*debut boucle while*/
     while( -(i + j) != 3){
         int k;
         printf(i);
     }
+    /*print*/
     printf(j);
 }
